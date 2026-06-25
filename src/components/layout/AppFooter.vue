c1cc92963e8a8358909ddc6d59affbd78c300587
Un-Known-Zz <2960772239@qq.com>
2026-06-25 20:19:35 +0800

    fix: remove conflicting GitHub Actions auto-deploy

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="footer-bottom">
-        <span>{{ t('footer.copyright') }} 1.03</span>
+        <span>{{ t('footer.copyright') }} 1.02</span>
       </div>
     </div>
   </footer>
