0be3818d2875abe2fc393622da00116f33a7f1e6
WorkBuddy <workbuddy@example.com>
2026-06-30 18:01:08 +0800

    feat: add wechat QR modal styles + bump version to 1.14

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="footer-bottom">
-        <span>{{ t('footer.copyright') }} 1.13</span>
+        <span>{{ t('footer.copyright') }} 1.14</span>
       </div>
     </div>
     <!-- 微信二维码弹窗 -->
